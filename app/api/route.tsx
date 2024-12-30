1092f39c6baba8b3a29b9b66f73aa86adb3305f4
shotoo999 <mohitshedge5@gmail.com>
2024-12-31 04:20:08 +0530

    removed test route

--- app/api/route.tsx
+++
@@ -1,6 +0,0 @@
-import { FetchSource } from "@/action/fetchApi";
-
-export async function GET() {
-  const episode = await FetchSource("your-name-10?ep=57910", true);
-  return Response.json(episode);
-}
