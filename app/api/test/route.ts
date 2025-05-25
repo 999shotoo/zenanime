61e1c6bb46cac87c3c0ccbf472e7ab4946ae49aa
999shotoo <mohitshedge5@gmail.com>
2025-05-25 18:34:24 +0530

    testing api on host

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -2,9 +2,9 @@
 import { META } from '@consumet/extensions';
 import Zoro from '@consumet/extensions/dist/providers/anime/zoro';
 
-const animeProvider = new META.Anilist(new Zoro());
+const animeProvider = new META.Anilist(new Zoro('https://hianimez.to/'));
 
 export async function GET(request: Request) {
-  const data = await animeProvider.fetchEpisodesListById('21')
+  const data = await animeProvider.fetchEpisodeSources('spy-x-family-17977$episode$89506$both')
   return Response.json({ data }, { status: 200 });
 }
